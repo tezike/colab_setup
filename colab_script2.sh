40155a2571137aa85b73a692d9ab2d158faa2fbf
tezike <31063326+tezike@users.noreply.github.com>
2020-09-17 01:23:18 +0100

    Update colab_script2.ssh

--- colab_script2.sh
+++ colab_script2.sh
@@ -1,3 +1,3 @@
 #perl -pi -e 's|execution_count": null|execution_count": 1|g' course-v4/nbs/*ipynb
 #jupyter labextension install @jupyter-widgets/jupyterlab-manager
-nohup jupyter notebook --no-browser --allow-root --ip="127.0.0.1" --port "6006"
+nohup jupyter notebook --no-browser --allow-root --ip="127.0.0.1" --port="6006"
